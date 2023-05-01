74ceac5ddfb3b0476a742afe089d7d15e54ad9ce
thehanir95 <thehanicis95@gmail.com>
2023-05-01 15:15:11 +0530

    changes

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,31 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="bg-gray-100 h-full">
+    <header class="bg-white shadow h-16 flex justify-between items-stretch">
+      <router-link :to="{name: 'home'}" class="inline-flex items-center h-full px-5">
+        Home
+      </router-link>
+      <div class="flex items-center gap-1">
+        <router-link to="/" class="inline-flex items-center px-2 h-full transition-colors hover:bg-purple-100">
+          Search Meals
+        </router-link>
+        <router-link to="/" class="inline-flex items-center px-2 h-full transition-colors hover:bg-purple-100">
+          Meals By Letter
+        </router-link>
+        <router-link to="/" class="inline-flex items-center px-2 h-full transition-colors hover:bg-purple-100">
+          Meals By Ingredients
+        </router-link>
+      </div>
+    </header>
+    <main>
+      <router-view />
+    </main>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
+<script setup>
+
+</script>
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
